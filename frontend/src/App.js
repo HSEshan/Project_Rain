001e0a19138bc4726c44ac35b4ca59e53bfe2bdd
Hossain Eshan <eshan.shahriar.77@gmail.com>
2025-06-02 11:46:29 -0400

    Part 2 of previous

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world! (Testing Tailwind)
+        </h1>
       </header>
     </div>
   );
